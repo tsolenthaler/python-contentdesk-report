a90043e003988b9b3f630081cb7e43535d6c76f3
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-08-22 11:24:41 +0200

    Bump version to 0.0.22 and improve URL checking logic in CheckUrlExist class

--- src/contentdeskreport/check/checkUrlExist.py
+++ src/contentdeskreport/check/checkUrlExist.py
@@ -12,10 +12,10 @@ class CheckUrlExist:
         checkProperty = 'url'
         for product in products:
             print(f"Checking Objekt: {product['identifier']}")
-        if checkProperty in product:
-            results.append({'sku': product['identifier'], 'uuid': product['uuid'], 'url': product['url']})
-        else:
-            print(f"No URL found for product: {product['identifier']}")
+            if checkProperty in product:
+                results.append({'identifier': product['identifier'], 'uuid': product['uuid'], 'url': product['url']})
+            else:
+                print(f"No URL found for product: {product['identifier']}")
         return results
 
     def loadProducts(self):
